b4c28bc36abd0f2158870a27e16e8e94315ef9cd
vadimistar <vadimistar@gmail.com>
2024-11-15 14:47:30 +0700

    init project

---
+++ src/main/java/com/vadimistar/effectivemobiletest/EffectiveMobileTestApplication.java
@@ -0,0 +1,13 @@
+package com.vadimistar.effectivemobiletest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectiveMobileTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EffectiveMobileTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vadimistar/effectivemobiletest/EffectiveMobileTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vadimistar.effectivemobiletest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectiveMobileTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
