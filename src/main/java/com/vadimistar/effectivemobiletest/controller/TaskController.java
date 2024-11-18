f1717ef11961f45b39bdc6437ccdc72c924523a2
vadimistar <vadimistar@gmail.com>
2024-11-18 21:29:17 +0700

    feat: add swagger docs for task endpoints

--- src/main/java/com/vadimistar/effectivemobiletest/controller/TaskController.java
+++ src/main/java/com/vadimistar/effectivemobiletest/controller/TaskController.java
@@ -1,10 +1,20 @@
 package com.vadimistar.effectivemobiletest.controller;
 
+import com.vadimistar.effectivemobiletest.dto.ErrorDto;
 import com.vadimistar.effectivemobiletest.dto.TaskDto;
 import com.vadimistar.effectivemobiletest.dto.UpdateTaskDto;
 import com.vadimistar.effectivemobiletest.entity.User;
 import com.vadimistar.effectivemobiletest.service.TaskService;
+import com.vadimistar.effectivemobiletest.util.PageableParameter;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -13,6 +23,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Задачи", description = "Управление задачами для всех пользователей")
 @SecurityRequirement(name = "JWT")
 @RestController
 @RequiredArgsConstructor
@@ -20,19 +31,72 @@ public class TaskController {
 
     private final TaskService taskService;
 
+    @Operation(summary = "Получить задание", description = "Получить задание по ID. Для этого пользователь должен быть исполнителем этого задания")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Задание", content = {
+                    @Content(schema = @Schema(implementation = TaskDto.class), mediaType = "application/json")
+            }),
+            @ApiResponse(responseCode = "400", description = "Неверный запрос", content = {
+                    @Content(schema = @Schema(implementation = ErrorDto.class), mediaType = "application/json")
+            }),
+            @ApiResponse(responseCode = "401", description = "Пользователь не авторизован, или неверный токен", content = {
+                    @Content(schema = @Schema(implementation = ErrorDto.class), mediaType = "application/json")
+            }),
+            @ApiResponse(responseCode = "404", description = "Задание не найдено", content = {
+                    @Content(schema = @Schema(implementation = ErrorDto.class), mediaType = "application/json")
+            }),
+            @ApiResponse(responseCode = "405", description = "Внутренняя ошибка сервера", content = {
+                    @Content(schema = @Schema(implementation = ErrorDto.class), mediaType = "application/json")
+            }),
+    })
     @GetMapping("/task/{id}")
-    public TaskDto getTask(@AuthenticationPrincipal User user, @PathVariable long id) {
+    public TaskDto getTask(@AuthenticationPrincipal User user,
+                           @Parameter(description = "ID задания") @PathVariable long id) {
         return taskService.getTask(user, id);
     }
 
+    @Operation(summary = "Получить задания", description = "Получить задания, которые исполняет пользователь")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Список с заданиями", content = {
+                    @Content(array = @ArraySchema(
+                            schema = @Schema(implementation = TaskDto.class)
+                    ), mediaType = "application/json")
+            }),
+            @ApiResponse(responseCode = "400", description = "Неверный запрос", content = {
+                    @Content(schema = @Schema(implementation = ErrorDto.class), mediaType = "application/json")
+            }),
+            @ApiResponse(responseCode = "401", description = "Пользователь не авторизован, или неверный токен", content = {
+                    @Content(schema = @Schema(implementation = ErrorDto.class), mediaType = "application/json")
+            }),
+            @ApiResponse(responseCode = "405", description = "Внутренняя ошибка сервера", content = {
+                    @Content(schema = @Schema(implementation = ErrorDto.class), mediaType = "application/json")
+            }),
+    })
+    @PageableParameter
     @GetMapping("/tasks")
-    public List<TaskDto> getTasks(@AuthenticationPrincipal User user, Pageable pageable) {
+    public List<TaskDto> getTasks(@AuthenticationPrincipal User user,
+                                  @Parameter(hidden = true) Pageable pageable) {
         return taskService.getTasks(user, pageable);
     }
 
+    @Operation(summary = "Обновить задание", description = "Обновить задание по ID. Для этого пользователь должен быть исполнителем этого задания")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Обновленное задание", content = {
+                    @Content(schema = @Schema(implementation = TaskDto.class), mediaType = "application/json")
+            }),
+            @ApiResponse(responseCode = "400", description = "Неверный запрос", content = {
+                    @Content(schema = @Schema(implementation = ErrorDto.class), mediaType = "application/json")
+            }),
+            @ApiResponse(responseCode = "401", description = "Пользователь не авторизован, или неверный токен", content = {
+                    @Content(schema = @Schema(implementation = ErrorDto.class), mediaType = "application/json")
+            }),
+            @ApiResponse(responseCode = "405", description = "Внутренняя ошибка сервера", content = {
+                    @Content(schema = @Schema(implementation = ErrorDto.class), mediaType = "application/json")
+            }),
+    })
     @PatchMapping("/task/{id}")
     public TaskDto updateTask(@AuthenticationPrincipal User user,
-                              @PathVariable long id,
+                              @Parameter(description = "ID задания") @PathVariable long id,
                               @Valid @RequestBody UpdateTaskDto updateTaskDto) {
         return taskService.updateTask(user, id, updateTaskDto);
     }
--- src/main/java/com/vadimistar/effectivemobiletest/dto/UpdateTaskDto.java
+++ src/main/java/com/vadimistar/effectivemobiletest/dto/UpdateTaskDto.java
@@ -1,16 +1,19 @@
 package com.vadimistar.effectivemobiletest.dto;
 
 import com.vadimistar.effectivemobiletest.entity.domain.Status;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Schema(description = "Запрос на обновление задания")
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class UpdateTaskDto {
 
+    @Schema(description = "Статус: pending (ожидание), in_process (в процессе) или done (выполнено)")
     private Status status;
 }
